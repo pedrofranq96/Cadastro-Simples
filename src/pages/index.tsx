7499738e93637e62c63732ea410c19cc25077340
pedrofranq96 <pedrofranq96@gmail.com>
2023-06-27 12:36:57 -0300

    alterações de estilo no layout, titulo e index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className={``}>
-      <span className="text-4xl">Texto</span>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
