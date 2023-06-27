0aad65f0ccd64677d79a580ce7968f6ea070d733
pedrofranq96 <pedrofranq96@gmail.com>
2023-06-27 15:59:37 -0300

    criando o componente botão e modificando a config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}', 
-    './src/components/**/*.{js,ts,jsx,tsx}'
-  ],
-  darkMode: false,
+
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+      'from-green-400',
+      'from-blue-400',
+      'from-gray-400',
+      'to-gray-700',
+      'to-blue-700',
+      'to-green-700',  
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
   theme: {
     extend: {},
   },
